3ecdc1d6765b0a075e994e2e6a91101e0d8d6727
Anmol Kumar <anmol.kumar@bigbinary.com>
2024-02-20 16:11:39 +0530

    Added delete button

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,21 @@
 class StudentsController < ApplicationController
+  before_action :load_student!, only: %i[show destroy]
+
   def index
     @students = Student.all
   end
 
   def show
 
+  end
+
+  def destroy
+    @student.destroy!
+  end
+
+  private
+
+  def load_student!
+    @student = Student.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :students, only: %i[index show]
+  resources :students, only: %i[index show destroy]
 end
